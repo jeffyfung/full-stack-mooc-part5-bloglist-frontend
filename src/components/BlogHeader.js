bee1509ea590683365261076db5d5c9d8cc57aed
Jeffy Fung <jeffyfungkh@gmail.com>
2022-05-25 00:03:53 +0800

    7.17: Navigation

--- src/components/BlogHeader.js
+++ src/components/BlogHeader.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { bindedActions as userBindedActions } from '../reducers/userReducer';
+import { Link } from 'react-router-dom';
 
 const BlogHeader = () => {
   const dispatch = useDispatch();
@@ -10,16 +11,21 @@ const BlogHeader = () => {
     dispatch(userBindedActions.clearUserInfo());
   };
 
+  const navBarStyle = { backgroundColor: 'grey', paddingTop: 7, paddingBottom: 7 };
+
   return (
-    <>
-      <h2>Blogs</h2>
-      <div>
-        {user.name} logged in{' '}
-        <button type='button' onClick={handleLogout}>
-          logout
-        </button>
-      </div>
-    </>
+    <div style={navBarStyle}>
+      <span style={{ margin: 2 }}>
+        <Link to={'/'}>blogs</Link>
+      </span>
+      <span style={{ margin: 2 }}>
+        <Link to={'/users'}>users</Link>
+      </span>
+      <span style={{ margin: 2 }}>{user.name} logged in </span>
+      <button type='button' onClick={handleLogout}>
+        logout
+      </button>
+    </div>
   );
 };
 
